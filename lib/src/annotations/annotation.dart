febf51db34a056e2c58ce9de4ac72f0ea4f6d300
victor <victorlopes9291@gmail.com>
2025-04-09 21:44:46 -0300

    feat: created the first annotations for testing

---
+++ lib/src/annotations/annotation.dart
@@ -0,0 +1,18 @@
+/// Annotations to Test, metadata to used to mark in methods and class as part of test.
+/// 
+
+library;
+/// Before each test, petal test will execute this (SetUp)
+class BeforeEach {
+  const BeforeEach();
+}
+
+/// After each test, petal test will execute this (SetUp)
+class AfterEach {
+  const AfterEach();
+}
+
+/// Way to say to petal test that a method is a test
+class Test {
+  const Test();
+}
