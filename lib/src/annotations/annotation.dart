91c6a713ac88c144b9fac04ac4496edb0d5a0a07
victor <victorlopes9291@gmail.com>
2025-04-11 23:14:21 -0300

    feat: created news annotations

--- lib/src/annotations/annotation.dart
+++ lib/src/annotations/annotation.dart
@@ -2,6 +2,9 @@
 /// 
 
 library;
+
+
+
 /// Before each test, petal test will execute this (SetUp)
 class BeforeEach {
   const BeforeEach();
@@ -26,4 +29,83 @@ class Disabled {
 class RepeatedTest{
   final int value ;
   const RepeatedTest({this.value  = 1});
+}
+
+class BeforeAll {
+  const BeforeAll();
+}
+
+
+class AfterAll {
+  const AfterAll();
+}
+
+class DisplayName {
+  final String displayName;
+  const DisplayName({this.displayName = ''});
+}
+
+class Only {
+  const Only();
+}
+
+
+class ValueSource<T> {
+  final List<int>? ints;
+  final List<double>? doubles;
+  final List<bool>? booleans;
+  final List<String>? strings;
+  final List<T>? classes;
+  final List<dynamic>? _currentData;
+  List<dynamic>? get currentData => _currentData;
+
+  const factory ValueSource.int(List<int>? ints) = ValueSource._ints;
+
+  const ValueSource._ints(this.ints) : 
+    doubles = null, 
+    booleans = null, 
+    strings = null, 
+    classes = null,
+    _currentData = ints;
+
+  const factory ValueSource.double(List<double>? doubles) = ValueSource._doubles;
+
+  const ValueSource._doubles(this.doubles) : 
+    ints = null, 
+    booleans = null, 
+    strings = null, 
+    classes = null,
+    _currentData = doubles;
+
+  const factory ValueSource.booleans(List<bool>? booleans) = ValueSource._booleans;
+
+  const ValueSource._booleans(this.booleans) : 
+    ints = null, 
+    doubles = null, 
+    strings = null, 
+    classes = null,
+    _currentData = booleans;
+
+  const factory ValueSource.strings(List<String>? strings) = ValueSource._strings;
+
+  const ValueSource._strings(this.strings) : 
+    ints = null, 
+    doubles = null, 
+    booleans = null, 
+    classes = null,
+    _currentData = strings;
+
+  const factory ValueSource.classes(List<T>? classes) = ValueSource._classes;
+
+  const ValueSource._classes(this.classes) : 
+    ints = null, 
+    doubles = null, 
+    booleans = null, 
+    strings = null,
+    _currentData = classes;
+}
+
+class ParameterizedTest {
+  final String displayName;
+  const ParameterizedTest({this.displayName = ''});
 }
